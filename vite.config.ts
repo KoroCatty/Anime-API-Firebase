219bcb68384c25a12b3fed596e8303bf6ac5f00f
K-sama <drumer19861018@gmail.com>
2024-02-24 21:57:32 +1100

    uninstalled dotenv package, coz VITE has it by default

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// To use Dotenv
-import { config } from 'dotenv'
-config()
 
-// vite標準のURLを localhost5173に変更し、firebase認証を使えるように設定
-import dns from 'dns';
-dns.setDefaultResultOrder('verbatim');
-
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
 })
